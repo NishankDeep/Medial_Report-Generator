46be2cfb5b00731811e11476dd7c2c2d97b3672e
Piyush-Raj01 <piyushraj.dto.2324@gmail.com>
2024-05-31 15:36:00 +0530

    Modified to latest User Demand

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ function App() {
     age: '',
     prn: '',
     gender: 'M',
-    date: '',
+    Sample_Colleted_On: '',
+    Sample_Out_On: '',
     referredBy: '',
     mainTestName: '',
   });
@@ -56,7 +57,8 @@ function App() {
       age: '',
       prn: '',
       gender: 'M',
-      date: '',
+      Sample_Colleted_On: '',
+      Sample_Out_On: '',
       referredBy: '',
       mainTestName: '',
     });
@@ -69,91 +71,95 @@ function App() {
   };
 
 
-const downloadReport = (index) => {
-  const report = reports[index];
-  setCurrentReport(report);
-  setShowPreview(true);
-
-  setTimeout(() => {
-    const input = previewRef.current;
-    if (input) {
-      html2canvas(input, { scale: 2 }).then((canvas) => {
-        const imgData = canvas.toDataURL('image/png');
-        const pdf = new jsPDF('p', 'mm', 'a4');
-        const imgWidth = 210; 
-        const pageHeight = 295; 
-        const imgHeight = (canvas.height * imgWidth) / canvas.width;
-        let heightLeft = imgHeight;
-        let position = 0;
-
-        // Custom header
-        const addHeader = (pdf, pageNumber) => {
-          pdf.addImage(MainLogo, 'JPEG', 10, 5, 25, 25); 
-          // pdf.addImage(mImg, 'JPEG', 10, 5, 25, 25); 
-          pdf.setFontSize(16);
-          pdf.text('SPARSH LAB', 40, 15);
-          pdf.setFontSize(10);
-          pdf.text('Sahitya Samaj Chowk, Jail road', 40, 20);
-          pdf.text('Daltonganj, 822101', 40, 25);
-          pdf.text('sparshclinicdaltonganj@gmail.com', 40, 30);
-          pdf.addImage(Microscope, 'PNG', 187, 12, 15, 15);
-          pdf.text('sparsh clinic daltonganj', 150, 25);
-          pdf.text('PHARMACY, LAB, CLINIC', 150, 30);
-        };
-
-        // Custom footer
-        const addFooter = (pdf, pageNumber) => {
-          pdf.setFontSize(10);
-          
-          pdf.text(':- END OF REPORT :-', 105, pageHeight - 20, { align: 'center' });
-          pdf.text('SAHITYA SAMAJ CHOWK, JAIL ROAD, DALTONGANJ', 105, pageHeight - 15, { align: 'center' });
-          pdf.text('PHONE NO - 9470944040, 9470944422', 105, pageHeight - 10, { align: 'center' });
-          pdf.setTextColor(255, 0, 0);
-          pdf.text('WISHING YOU A GOOD LIFE AND BE HEALTHY', 105, pageHeight - 5, { align: 'center' });
-          pdf.setTextColor(0, 0, 0);
-        };
-
-        addHeader(pdf, 1);
-        // pdf.addImage(imgData, 'JPEG', 0, 35, imgWidth, imgHeight);
-        pdf.addImage(imgData, 'PNG', 0, 35, imgWidth, imgHeight);
-        heightLeft -= pageHeight;
-
-        addFooter(pdf, 1);
-
-        let pageNumber = 2;
-        while (heightLeft >= 0) {
-          position = heightLeft - imgHeight;
-          pdf.addPage();
-          addHeader(pdf, pageNumber);
-          pdf.addImage(imgData, 'PNG', 0, position + 35, imgWidth, imgHeight);
+  const downloadReport = (index) => {
+    const report = reports[index];
+    setCurrentReport(report);
+    setShowPreview(true);
+  
+    setTimeout(() => {
+      const input = previewRef.current;
+      if (input) {
+        html2canvas(input, { scale: 2 }).then((canvas) => {
+          const imgData = canvas.toDataURL('image/png');
+          const pdf = new jsPDF('p', 'mm', 'a4');
+          const imgWidth = 210;
+          const pageHeight = 295;
+          const imgHeight = (canvas.height * imgWidth) / canvas.width;
+          let heightLeft = imgHeight;
+          let position = 0;
+  
+          // Custom header
+          const addHeader = (pdf, pageNumber) => {
+            pdf.addImage(MainLogo, 'JPEG', 10, 5, 25, 25);
+            pdf.setFontSize(40);
+            pdf.setFont('helvetica', 'bold');
+            pdf.text('SPARSH LAB', 40, 15);
+            pdf.setFontSize(10);
+            pdf.text('Sahitya Samaj Chowk, Jail road', 40, 20);
+            pdf.text('Daltonganj, 822101', 40, 25);
+            pdf.text('Email : sparshclinicdaltonganj@gmail.com', 40, 30);
+            pdf.addImage(Microscope, 'PNG', 187, 12, 15, 15);
+            pdf.text('sparsh clinic daltonganj', 150, 25);
+            pdf.text('PHARMACY, LAB, CLINIC', 150, 30);
+            pdf.setLineWidth(1.5);
+            pdf.line(10, 35, 200, 35); 
+          };
+  
+          // Custom footer
+          const addFooter = (pdf, pageNumber) => {
+            pdf.setFont('helvetica', 'bold');
+            pdf.text('SAHITYA SAMAJ CHOWK, JAIL ROAD, DALTONGANJ', 105, pageHeight - 15, { align: 'center' });
+            pdf.text('PHONE NO - 9470944040, 9470944422', 105, pageHeight - 10, { align: 'center' });
+            pdf.setTextColor(255, 0, 0);
+            pdf.text('WISHING YOU A GOOD LIFE AND BE HEALTHY', 105, pageHeight - 5, { align: 'center' });
+            pdf.setTextColor(0, 0, 0);
+            pdf.setFontSize(10);
+            pdf.text(':- END OF REPORT :-', 105, pageHeight - 20, { align: 'center' });
+          };
+  
+          addHeader(pdf, 1);
+          pdf.addImage(imgData, 'PNG', 0, 35, imgWidth, imgHeight);
           heightLeft -= pageHeight;
-          addFooter(pdf, pageNumber);
-          pageNumber++;
-        }
-
-        pdf.save('report.pdf');
-        setShowPreview(false);
-      });
-    }
-  }, 500);
-};
-
   
+          addFooter(pdf, 1);
   
-
-  useEffect(() => {
-    if (showPreview) {
-      const handleClickOutside = (event) => {
-        if (modalRef.current && !modalRef.current.contains(event.target)) {
+          let pageNumber = 2;
+          while (heightLeft >= 0) {
+            position = heightLeft - imgHeight;
+            pdf.addPage();
+            addHeader(pdf, pageNumber);
+            pdf.addImage(imgData, 'PNG', 0, position + 35, imgWidth, imgHeight);
+            heightLeft -= pageHeight;
+            addFooter(pdf, pageNumber);
+            pageNumber++;
+          }
+  
+         
+          pdf.save('report.pdf');
           setShowPreview(false);
-        }
-      };
-      document.addEventListener('mousedown', handleClickOutside);
-      return () => {
-        document.removeEventListener('mousedown', handleClickOutside);
-      };
-    }
-  }, [showPreview]);
+        });
+      }
+    }, 500);
+  };
+  
+
+  
+  
+
+useEffect(() => {
+  if (showPreview) {
+    const handleClickOutside = (event) => {
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+        setShowPreview(false);
+      }
+    };
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }
+}, [showPreview]);
+
 
   const isValueOutOfRange = (result, bioRefInterval) => {
     const [min, max] = bioRefInterval.split('-').map(Number);
@@ -215,7 +221,10 @@ const downloadReport = (index) => {
             </select>
           </div>
           <div>
-            <label className="block mb-1">Date:</label>
+          </div>
+        </div>
+        <div className="grid grid-cols-2 gap-4">
+            <label className="block mb-1">Sample Colleted On : </label>
             <input
               type="date"
               name="date"
@@ -225,7 +234,17 @@ const downloadReport = (index) => {
               className="w-full p-2 border border-gray-300 rounded"
             />
           </div>
-        </div>
+          <div>
+            <label className="block mb-1">Report Out On ::</label>
+            <input
+              type="date"
+              name="date"
+              value={formData.date}
+              onChange={handleInputChange}
+              required
+              className="w-full p-2 border border-gray-300 rounded"
+            />
+          </div>
         <div className="grid grid-cols-2 gap-4">
           <div>
             <label className="block mb-1">Referred By:</label>
@@ -326,8 +345,9 @@ const downloadReport = (index) => {
             <p>PRN No.: {report.prn}</p>
             <p>Gender: {report.gender}</p>
             <p>Date: {report.date}</p>
+            <p>Date: {report.date}</p>
             <p>Referred By: {report.referredBy}</p>
-            <p>Main Test Name: {report.mainTestName}</p>
+            <p>{report.mainTestName}</p>
             <table className="w-full mt-4 border-collapse border border-gray-300">
               <thead>
                 <tr>
@@ -382,69 +402,56 @@ const downloadReport = (index) => {
             className="bg-white p-8 rounded shadow-lg w-3/4 max-h-full overflow-auto relative "
           >
             <div className="header flex justify-between items-center mb-4 ml-8 mr-8">
-  {/* <div className="left">
-    <div className="header flex justify-center items-center mb-4">
-      <div className="flex items-center">
-        <img src="MainLogo.jpeg" alt="Clinic Logo" className="h-12 w-auto" />
-        <h1 className="text-5xl font-bold mt-2 ml-4">SPARSH LAB</h1>
-      </div>
-    </div>
-    <div className="top-space mt-6">
-      <p>Sahitya Samaj Chowk, Jail road</p>
-      <p>Daltonganj, 822101</p>
-      <p>sparshclinicdaltonganj@gmail.com</p>
-    </div>
-  </div>
-  <div className="right flex flex-col">
-  <p className="font-bold mb-2">sparsh clinic daltonganj</p>
-  <img src="Microscope.png" alt="Clinic Logo" className="h-12 w-auto mr-2" />
-  <div className="flex items-center">
-    
-    <p>PHARMACY, LAB, CLINIC</p>
-  </div>
-</div> */}
 
 
 </div>
+<div className="flex justify-between mb-4 ml-4 mr-4">  {/* Reduced ml and mr to 4 */}
+  {/* <h2 className="text-xl font-semibold  mr-8">Final Report</h2> */}
+  {/* <button
+    onClick={() => setShowPreview(false)}
+    className="px-4 py-2 bg-red-500 text-white rounded"
+  >
+    Close
+  </button> */}
+</div>
+<div className="flex flex-col-2 justify-center space-x-96 mb-4 font-bold text-xl p-4">
 
-            <div className="flex justify-between mb-4 ml-8 mr-8">
-            <h2 className="text-xl font-semibold  mr-8">Final Report</h2>
-              <button
-                onClick={() => setShowPreview(false)}
-                className="px-4 py-2 bg-red-500 text-white rounded"
-              >
-                Close
-              </button>
-            </div>
-            <div className="grid grid-cols-2 gap-4 mb-4 ml-8 mr-8">
-              <div>
-                <p>
-                  <strong>Name:</strong> {currentReport.name}
-                </p>
-                <p>
-                  <strong>Gender:</strong> {currentReport.gender}
-                </p>
-                <p>
-                  <strong>Referred By:</strong> {currentReport.referredBy}
-                </p>
-              </div>
-              <div className="text-right">
-                <p>
-                  <strong>PRN No:</strong> {currentReport.prn}
-                </p>
-                <p>
-                  <strong>Date:</strong> {currentReport.date}
-                </p>
-              </div>
-            </div>
+  <div>
+    <p>
+      <strong>Name:</strong> {currentReport.name}
+    </p>
+    <p>
+      <strong>Age:</strong> {currentReport.age}
+    </p>
+    <p>
+      <strong>Gender:</strong> {currentReport.gender}
+    </p>
+    <p>
+      <strong>Referred By:</strong> {currentReport.referredBy}
+    </p>
+  </div>
+  <div className="text-right">
+    <p>
+      <strong>PRN No:</strong> {currentReport.prn}
+    </p>
+    <p>
+      <strong>Sample Colleted On:</strong> {currentReport.date}
+    </p>
+    <p>
+      <strong>Report Out On:</strong> {currentReport.date}
+    </p>
+  </div>
+</div>
+
             
-            <div className="border border-gray-300 p-2 font-semibold text-2xl text-center">Main Test Name</div>
-            <div className="border border-gray-300 p-2 text-center">{currentReport.mainTestName}</div>
+            {/* <div className="border border-gray-300 p-2 font-semibold text-2xl text-center">Main Test Name</div> */}
+            <div className=" mt-8 p-2 font-semibold text-5xl text-center">{currentReport.mainTestName}</div>
 
             
 {/* Result Table */}
 <div className="overflow-x-auto ml-8 mr-8">
-  <table className="w-full mt-16 border-collapse border-collapse border-gray-300">
+  {/* <table className="w-full mt-16 border-collapse border-collapse border-gray-300"> */}
+  <table className="w-full mt-8 ">
     <thead>
       <tr>
         <th className="border border-gray-300 p-2">Test Name</th>
@@ -457,7 +464,7 @@ const downloadReport = (index) => {
       {currentReport.tests.map((test, index) => (
         <tr key={index}>
           <td className="border border-gray-300 p-2">{test.testName}</td>
-          <td className={`border border-gray-300 p-2 ${isValueOutOfRange(test.result, test.bioRefInterval) ? 'bg-red-200 font-bold' : 'font-bold'}`}>
+          <td className={`border border-gray-300 p-2 ${isValueOutOfRange(test.result, test.bioRefInterval) ? 'bg-red-200 font-bold' : ''}`}>
             {test.result}
           </td>
           <td className="border border-gray-300 p-2">{test.units}</td>
@@ -468,12 +475,17 @@ const downloadReport = (index) => {
   </table>
 </div>
 
-            {/* <footer className="mt-20 mb-4 border-t border-gray-300 pt-2 text-center">
-              <p className="text-lg font-semibold">:- END OF REPORT :-</p>
-              <p>SAHITYA SAMAJ CHOWK, JAIL ROAD, DALTONGANJ</p>
-              <p>PHONE NO - 9470944040, 9470944422</p>
-              <p className="text-red-500">WISHING YOU A GOOD LIFE AND BE HEALTHY</p>
-            </footer> */}
+<div className="flex justify-end  mt-8 space-x-28">
+      <div className="flex flex-col items-center">
+        <img src="ManishSign.jpg" alt="Lab Technician" className="w-16 h-16 object-cover mb-2"/>
+        <p className="font-bold">LAB TECHNICIAN</p>
+      </div>
+      <div className="flex flex-col items-center">
+        <img src="DoctorSign.PNG" alt="Dr. Aubhuti Choudhary" className="w-16 h-16 object-cover mb-2"/>
+        <p className="font-bold">DR. Aubhuti Choudhary</p>
+        <p>M.D Pathology</p>
+      </div>
+    </div>
           </div>
         </div>
       )}
@@ -483,6 +495,3 @@ const downloadReport = (index) => {
 
 export default App;
 
-
-  
-                
